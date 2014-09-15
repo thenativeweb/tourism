2a83dfe84dada9c904f9ba8af6590a35b6b6a0d3
Michael Scherer <michael.scherer@sealsystems.de>
2014-09-15 14:15:51 +0200

    Adjust paths

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -3,4 +3,4 @@
 filterRegexp=$1
 
 # Run npm update only if new versions found
-node_modules/npm-check-updates/bin/npm-check-updates -u -f $filterRegexp | tee >(grep -q "All dependencies match the latest package versions :)" || npm update)
+node_modules/tourism/node_modules/npm-check-updates/bin/npm-check-updates -u -f $filterRegexp | tee >(grep -q "All dependencies match the latest package versions :)" || npm update)
