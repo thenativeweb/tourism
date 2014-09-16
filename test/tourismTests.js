85d1e834aeda9ebbc5238146bcdea6f9be814b1f
Golo Roden <webmaster@goloroden.de>
2014-09-16 09:27:38 +0200

    Enabled async-only tests. Closes #5.

--- test/tourismTests.js
+++ test/tourismTests.js
@@ -5,7 +5,8 @@ var assert = require('node-assertthat');
 var tourism = require('../lib/tourism');
 
 suite('tourism', function () {
-  test('is a function.', function () {
+  test('is a function.', function (done) {
     assert.that(tourism, is.ofType('function'));
+    done();
   });
 });
